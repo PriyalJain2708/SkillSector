b4466783d9bb5eaf68d1d96db2c7222f8dadc326
Priyal Jain <69732720+PriyalJain2708@users.noreply.github.com>
2021-01-29 07:12:37 -0800

    Add files via upload

---
+++ ASP .NET CORE API/ASP .NET CORE API/ASP .NET CORE API/Models/ProgrammerInformation.cs
@@ -0,0 +1,25 @@
+﻿#nullable disable
+
+namespace SkillSectorCoreApi.Models
+{
+    public partial class ProgrammerInformation
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public string Type { get; set; }
+        public string Imagesrc { get; set; }
+        public string Resume { get; set; }
+        public string Links { get; set; }
+        public string Location { get; set; }
+        public string City { get; set; }
+        public string StreetAddress { get; set; }
+        public string Zipcode { get; set; }
+        public string Skills { get; set; }
+        public string Tags { get; set; }
+        public string Date { get; set; }
+    }
+}
